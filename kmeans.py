7f2f4c39be694d992cb0c20bf71b58653b48a8e5
lucidrains <lucidrains@gmail.com>
2025-04-29 10:07:56 -0700

    add a genetic and kmeans hybrid version

--- kmeans.py
+++ kmeans.py
@@ -6,6 +6,9 @@ from einops import einsum, rearrange, repeat, reduce
 
 from sklearn.datasets import make_blobs
 
+def exists(v):
+    return v is not None
+
 # metric, using silhouette score
 
 def silhouette_score(clusters, points):
@@ -17,30 +20,35 @@ def silhouette_score(clusters, points):
 # regular kmeans
 
 @torch.inference_mode()
-def kmeans(points, num_clusters):
+def kmeans(points, num_clusters, centers = None, max_iterations = float('inf')):
     num_points, dim = points.shape
 
     # init centers
 
-    centers = torch.randn(num_clusters, dim)
+    iterations = 0
 
-    while True:
+    if not exists(centers):
+        centers = torch.randn(num_clusters, dim)
+
+    while iterations < max_iterations:
         # assign points to a center
 
         dist = torch.cdist(points, centers)
 
         center_ids = dist.argmin(dim = -1)
-        expanded_center_ids = repeat(center_ids, 'n -> n d', d = dim)
+        expanded_center_ids = repeat(center_ids, '... n -> ... n d', d = dim)
+
+        expanded_points = repeat(points, 'n d -> p n d', p = center_ids.shape[0]) if expanded_center_ids.ndim == 3 else points
 
         # average intra cluster distance
 
-        next_centers = torch.zeros_like(centers).scatter_reduce_(0, expanded_center_ids, points, 'mean')
+        next_centers = torch.zeros_like(centers).scatter_reduce_(-2, expanded_center_ids, expanded_points, 'mean')
 
         # account for empty clusters by not changing
 
-        is_empty_cluster = einx.not_equal('c, n -> c n', torch.arange(num_clusters), center_ids).all(dim = -1)
+        is_empty_cluster = einx.not_equal('c, ... n -> ... c n', torch.arange(num_clusters), center_ids).all(dim = -1)
 
-        next_centers = einx.where('c, c d, c d', is_empty_cluster, centers, next_centers)
+        next_centers = einx.where('... c, ... c d, ... c d', is_empty_cluster, centers, next_centers)
 
         # stop if no changes between iterations
 
@@ -49,19 +57,22 @@ def kmeans(points, num_clusters):
 
         centers.copy_(next_centers)
 
+        iterations += 1
+
     return centers
 
 # kmeans with genetic algorithm
 
 @torch.inference_mode()
-def genetic_kmeans(
+def genetic_clustering(
     points,
     num_clusters,
     pop_size = 250,
     num_generations = 100,
     frac_fittest_selected = 0.25,
     frac_elites = 0.1,
-    mutation_strength = 0.1
+    mutation_strength = 0.1,
+    kmeans_each_generation = False
 ):
     num_points, dim = points.shape
     keep_fittest = int(frac_fittest_selected * pop_size)
@@ -76,6 +87,9 @@ def genetic_kmeans(
     # generations
 
     for _ in range(num_generations):
+
+        if kmeans_each_generation:
+            pop_centers = kmeans(points, num_clusters, pop_centers, max_iterations = 1)
 
         fitness = silhouette_score(pop_centers, points)
 
@@ -120,8 +134,10 @@ def genetic_kmeans(
 # running and evaluating
 
 num_trials = 10
+
 avg_kmeans = 0.
 avg_genetic = 0.
+avg_genetic_kmeans = 0.
 
 for _ in range(num_trials):
 
@@ -138,16 +154,23 @@ for _ in range(num_trials):
 
     kmeans_score = silhouette_score(clusters, points)
 
-    clusters = genetic_kmeans(points, 6)
+    clusters = genetic_clustering(points, 6)
 
     genetic_score = silhouette_score(clusters, points)
 
+    clusters = genetic_clustering(points, 6, kmeans_each_generation = True)
+
+    genetic_kmeans_score = silhouette_score(clusters, points)
+
     avg_kmeans += kmeans_score / num_trials
     avg_genetic += genetic_score / num_trials
+    avg_genetic_kmeans += genetic_kmeans_score / num_trials
 
 # print scores
 
-print(f'\nscores over {num_trials} trials: (higher better)')
+print(f'\nscores over {num_trials} trials: (higher better)\n')
 
 print(f'kmeans: {avg_kmeans:.4f}')
-print(f'genetic kmeans: {avg_genetic:.4f}')
+print(f'genetic clustering: {avg_genetic:.4f}')
+print(f'genetic kmeans hybrid: {avg_genetic_kmeans:.4f}')
+print('\n')
